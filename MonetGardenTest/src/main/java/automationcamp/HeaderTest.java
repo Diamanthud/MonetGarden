16bd6638f5ee21054f000564af393aa49cf0e3f1
Diamanthud <nsantana91@hotmail.com>
2023-10-03 11:50:13 +0200

    Huvudvärk

--- MonetGardenTest/src/main/java/automationcamp/HeaderTest.java
+++ MonetGardenTest/src/main/java/automationcamp/HeaderTest.java
@@ -21,6 +21,7 @@ public class HeaderTest extends TestBase{
 		String loginUrl = "https://monets-garden.se";
 		page.navigate(loginUrl);		
 		
+		// Accept Cookies
 		Locator acceptCookies = page.getByRole(AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName("Jag accepterar"));
         if (acceptCookies.isVisible()) {
@@ -32,9 +33,8 @@ public class HeaderTest extends TestBase{
         page.click("button.Header_listButton__GUrK4");
         // Vänta på att listan med städer ska visas 
         page.waitForSelector(".Header_innerList__wuB1y");
-        page.click("a[href='/stockholm']");
+        page.click("a[href='stockholm']");
            assertThat(page).hasURL("https://monets-garden.se/stockholm");
-
           page.goBack();
         
     	// Hitta och klicka på knappen "Städer" igen
@@ -45,20 +45,43 @@ public class HeaderTest extends TestBase{
            assertThat(page).hasURL("https://monets-garden.se/malmo");
           page.goBack();  
         
+          
         // ------GALLERY------	
 		page.click("text=Galleri");
 	        assertThat(page).hasURL("https://monets-garden.se/gallery");
 		  page.goBack();
+		  
 		  
         // ------VANLIGA FRÅGOR------ 
 		page.click("text=Vanliga frågor");
 	        assertThat(page).hasURL("https://monets-garden.se/#vanliga-fragor");
 		  page.goBack();
   
+		  
 		// ------KONTAKT------
 		page.click("text=kontakt");
 	        assertThat(page).hasURL("https://monets-garden.se/#kontakt");
 		  page.goBack();
+		  
+		// ------BILJETTER------  
+		page.click("button.Header_ctaButton__9S7Hn");
+        page.waitForSelector(".Header_innerList__wuB1y");
+        page.click("a[aria-label='Stockholm']");
+        assertThat(page).hasURL("https://www.eventim.se/eventseries/3289035/?affiliate=FKS&_gl=1*bh7hfz*_gcl_au*MTU3NjEzNDc2NC4xNjk1OTY5MTk1");
+        page.goBack();
+        
+        /*
+        page.click("a[href='/Stockholm']");
+           assertThat(page).hasURL("https://www.eventim.se/eventseries/3289035/?affiliate=FKS&_gl=1*bh7hfz*_gcl_au*MTU3NjEzNDc2NC4xNjk1OTY5MTk1");
+          page.goBack();*/
+          
+		  /*
+          page.click("button.Header_ctaButton__9S7Hn");
+          page.waitForSelector(".Header_innerList__wuB1y");
+          await page.waitForSelector("a[href='/Stockholm']", { visible: true });
+          page.click("a[href='/Stockholm']");
+          assertThat(page).hasURL("https://www.eventim.se/eventseries/3289035/?affiliate=FKS&_gl=1*bh7hfz*_gcl_au*MTU3NjEzNDc2NC4xNjk1OTY5MTk1");
+          page.goBack();*/
 		  
 		  
         /* Pausa i några sekunder för att observera effekten (kan användas för felsökning)
